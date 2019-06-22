3adc820387cd7c8a308364a25a422d630c16fa66
Kléber de Oliveira Andrade <pdjkleber@gmail.com>
2019-06-22 12:43:29 -0300

    Create uri1002.py

---
+++ uri1002.py
@@ -0,0 +1,11 @@
+# define o valor de pi conforme o problema
+pi = 3.14159
+
+# lê a entrada (raio do circulo)
+raio = float(input())
+
+# calcula a área do circulo
+area = pi * (raio**2)
+ 
+# exibe o resultado
+print ("A=%.4f" % (area))
