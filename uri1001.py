b562a20811922d57eee959e1f9eac093ff7a631b
Kléber de Oliveira Andrade <pdjkleber@gmail.com>
2019-06-22 12:43:25 -0300

    Create uri1001.py

---
+++ uri1001.py
@@ -0,0 +1,10 @@
+# lê a primeira entrada
+a = int(input())
+# lê a segunda entrada
+b = int(input())
+
+# soma os dois valores A + B
+x = a + b
+
+# exibe o resultado na tela com a formatação pedida
+print('X = {}'.format(x))
